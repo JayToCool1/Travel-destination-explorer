4671d701d422d9850baf5744dd70be220413d47c
JayToCool1 <164425173+JayToCool1@users.noreply.github.com>
2025-05-15 19:30:02 -0400

    Commit Updated Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Destinations from "./pages/Destinations";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div style={{ paddingTop: "56px" }}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/destinations" element={<Destinations />} />
         </Routes>
       </div>
     </Router>
