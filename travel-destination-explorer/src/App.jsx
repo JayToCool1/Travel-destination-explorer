1255f2f8a117f98506825cc2f1b716834a6db60f
JayToCool1 <164425173+JayToCool1@users.noreply.github.com>
2025-05-20 16:25:22 -0400

    Updated App.jsx

--- travel-destination-explorer/src/App.jsx
+++ travel-destination-explorer/src/App.jsx
@@ -1,24 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Favorites from "./pages/Favorites";
-import Destinations from "./pages/Destinations";
 import Quiz from "./components/Quiz";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <div style={{ paddingTop: "56px" }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/destinations" element={<Destinations />} />
-          <Route path="/quiz" element={<Quiz />} />
-        </Routes>
-      </div>
-    </Router>
+    <div style={{ padding: "40px" }}>
+      <Quiz />
+    </div>
   );
 }
 
